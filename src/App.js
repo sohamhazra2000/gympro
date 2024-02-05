374e5951ff2561165ffdff475c9ab674e944525a
sohamhazra2000 <sohamhazra15@gmail.com>
2024-02-05 11:18:56 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 
+import { Suspense, lazy } from 'react';
 import './App.css';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import Home from"./Home"
-import Serviced from './Serviced';
-import Login from './Login';
-import Register from './Register';
-import Blogd from './Blogd';
-import ResponsiveAppBar from './Nav';
-import Trainer from './Trainer';
-import StickyFooter from './Footer';
+// import Home from"./Home"
+// import Serviced from './Serviced';
+// import Login from './Login';
+// import Register from './Register';
+// import Blogd from './Blogd';
+// import ResponsiveAppBar from './Nav';
+// import Trainer from './Trainer';
+// import StickyFooter from './Footer';
+const Home = lazy(() => import("./Home"));
+const Serviced = lazy(() => import("./Serviced"));
+const Login = lazy(() => import("./Login"));
+const Register = lazy(() => import("./Register"));
+const Blogd=lazy(() => import("./Blogd"))
+const ResponsiveAppBar=lazy(() => import("./Nav"))
+const Trainer=lazy(() => import("./Trainer"))
+const StickyFooter=lazy(() => import("./Footer"))
+
 
 function App() {
 
   return (
+    <Suspense fallback={<h2>Loading.....</h2>}>
     <Router>
        <ResponsiveAppBar/>
       <Routes>
@@ -28,6 +39,7 @@ function App() {
       </Routes>
       <StickyFooter/>
     </Router>
+    </Suspense>
     
       
   );
